084bfd75818b242fcb4ed685541ed34f1f37ef39
wojciechantos <wojciech.antos1990@gmail.com>
2024-06-30 14:25:03 +0200

    Dialog improvements implemented, minor cleanup

--- src/components/Dialog/index.ts
+++ src/components/Dialog/index.ts
@@ -9,21 +9,24 @@ export class Dialog {
 		const appContainer: HTMLElement = document.getElementById('app-container')!;
 		const dialogElement: HTMLDialogElement = appContainer.querySelector<HTMLDialogElement>('.dialog')!;
 		const oldShow = dialogElement.showModal;
+		const oldClose = dialogElement.close;
+
 		dialogElement.showModal = () => {
 			oldShow.call(dialogElement);
 			dialogElement.classList.add('show');
 		};
-		const oldClose = dialogElement.close;
+
 		dialogElement.close = () => {
-			dialogElement.addEventListener('transitionend', () => oldClose.call(dialogElement), { once: true });
 			dialogElement.classList.remove('show');
+			setTimeout(() => oldClose.call(dialogElement), 20)
 		};
+
 		return dialogElement;
 	}
 
 	public render(): HTMLElement {
-		const addNewBookButton = new Button({ text: 'X', onClick: () => this.getDialog().close() });
-		const addNewBookButtonElement: HTMLElement = addNewBookButton.render();
+		const closeDialogButton = new Button({ text: 'X', onClick: () => this.getDialog().close() });
+		const closeDialogButtonElement: HTMLElement = closeDialogButton.render();
 
 		const dialogElement: HTMLDialogElement = document.createElement('dialog')!;
 		dialogElement.classList.add('dialog');
@@ -38,7 +41,7 @@ export class Dialog {
 			</div>
 		`;
 
-		dialogElement.querySelector('.dialog__header').appendChild(addNewBookButtonElement);
+		dialogElement.querySelector('.dialog__header').appendChild(closeDialogButtonElement);
 
 		return dialogElement;
 	}
