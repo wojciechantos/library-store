ea66d3d2acd0e4f1683fcf205c4b0906f456dcd6
wojciechantos <wojciech.antos1990@gmail.com>
2024-07-05 14:03:50 +0200

    Vitest added for unit testing and configured; Tests run added to 'build' command; Button unit tests implemented

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+	test: {
+		globals: true,
+		environment: 'jsdom',
+	},
 	plugins: [tsconfigPaths()],
 });
